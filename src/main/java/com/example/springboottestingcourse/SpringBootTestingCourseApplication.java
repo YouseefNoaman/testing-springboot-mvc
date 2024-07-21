c04f00583e66bedccf03cafbdb7ecc8f6e9ff585
yosef <youseefnoaman@gmail.com>
2024-07-21 20:16:00 +0100

    This is the boot of the project

---
+++ src/main/java/com/example/springboottestingcourse/SpringBootTestingCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboottestingcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTestingCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTestingCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboottestingcourse/SpringBootTestingCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboottestingcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestingCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
